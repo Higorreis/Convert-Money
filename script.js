130b9086522048e4fd452a2e761c4f9d8f36bca0
Higorreis <higorreis17@gmail.com>
2025-05-14 00:35:01 -0300

    Atualização Index e Script

--- script.js
+++ script.js
@@ -5,65 +5,144 @@ function convertsValues() {
     //Trasformando em Float
     const formatEnteredValue = parseFloat(enteredValue.value)
     //Valor Real Brasileiro
-    const brazilianReal = document.querySelector("#brazil-value")
+    const valueConvertsIt = document.querySelector("#value-converts-it")
     //Valor Estrangeiro
     const foreignerValue = document.querySelector("#foreigner-value")
+
 
     //Try- Catch
     try {
         //Formatando o REAL
         if (!isNaN(formatEnteredValue)) {
-            //Formatar o valor Digitado pelo Usuário
-            const realFormatted = new Intl.NumberFormat('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'
-            }).format(formatEnteredValue)
 
-            //Mostrar valor Real Brasileiro formatado
-            brazilianReal.innerHTML = realFormatted
+            if (primarySelect.value === "BRL") {
+                //Formatar o valor Digitado pelo Usuário
+                valueConvertsIt.innerHTML = new Intl.NumberFormat('pt-BR', {
+                    style: "currency",
+                    currency: "BRL"
+                }).format(formatEnteredValue)
 
-            //Condição para DOLAR
-            if (select.value === "USD") {
-                const valueDolar = enteredValue.value / 5.65
-                const valueDolarFor = new Intl.NumberFormat('en-US', {
+                if (secondSelect.value === "BRL") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+                        style: "currency",
+                        currency: "BRL"
+                    }).format(formatEnteredValue)
+                }
+
+                //Condição para DOLAR
+                if (secondSelect.value === "USD") {
+
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(enteredValue.value / 5.65)
+                    //Mostrar valor Estrangeiro Formatado
+                }
+                //Condição para EURO
+                if (secondSelect.value === "EUR") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR'
+                    }).format(enteredValue.value / 6.37)
+                }
+
+                //Condição para Libra
+                if (secondSelect.value === "LIB") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP'
+                    }).format(enteredValue.value / 7.51)
+                }
+
+                //Condição para Bitcoin
+                if (secondSelect.value === "BIT") {
+                    //const valueBitcoin = en-US
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'decimal',
+                        minimumFractionDigits: 8,
+                        maximumFractionDigits: 8
+                    }).format(enteredValue.value / 581.494, 51)
+                }
+            }
+            if (primarySelect.value === "USD") {
+                valueConvertsIt.innerHTML = new Intl.NumberFormat('en-US', {
                     style: 'currency',
                     currency: 'USD'
-                }).format(valueDolar)
-                //Mostrar valor Estrangeiro Formatado
-                foreignerValue.innerHTML = valueDolarFor
+                }).format(formatEnteredValue)
 
-            } 
-            //Condição para EURO
-            if (select.value === "EUR") {
-                const valueEuro = enteredValue.value / 6.37
-                const valueEuroFor = new Intl.NumberFormat('de-DE', {
+                if (secondSelect.value === "USD") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(formatEnteredValue)
+                }
+                if (secondSelect.value === "BRL") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(enteredValue.value * 5.65)
+                }
+                if (secondSelect.value === "EUR") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR'
+                    }).format(enteredValue.value * 0.89)
+                }
+                if (secondSelect.value === "LIB") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP'
+                    }).format(enteredValue.value * 0.75)
+                }
+                if (secondSelect.value === "BIT") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'decimal',
+                        minimumFractionDigits: 8,
+                        maximumFractionDigits: 8
+                    }).format(enteredValue.value * 0.0000096)
+                }
+            }
+            if (primarySelect.value === "EUR") {
+                valueConvertsIt.innerHTML = new Intl.NumberFormat('de-DE', {
                     style: 'currency',
                     currency: 'EUR'
-                }).format(valueEuro)
-                foreignerValue.innerHTML = valueEuroFor
+                }).format(formatEnteredValue)
+
+                if (secondSelect.value === "EUR") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR'
+                    }).format(formatEnteredValue)
+                }
+                if (secondSelect.value === "BRL") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(enteredValue.value * 6.27)
+                }
+                if (secondSelect.value === "USD") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(enteredValue.value * 1.12)
+                }
+                if (secondSelect.value === "LIB") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP'
+                    }).format(enteredValue.value * 0.84)
+                }
+                if (secondSelect.value === "BIT") {
+                    foreignerValue.innerHTML = new Intl.NumberFormat('en-US', {
+                        style: 'decimal',
+                        minimumFractionDigits: 8,
+                        maximumFractionDigits: 8
+                    }).format(enteredValue.value * 0.000011)
+                }
             }
-            //Condição para Libra
-            if(select.value === "LIB"){
-                const valueLibra = enteredValue.value / 7.51
-                const valueLibraFor = new Intl.NumberFormat('en-GB',{
-                    style: 'currency',
-                    currency: 'GBP'
-                }).format(valueLibra)
-                foreignerValue.innerHTML = valueLibraFor
-            }
-            //Condição para Bitcoin
-            if(select.value === "BIT"){
-                const valueBitcoin = enteredValue.value / (581.494,51)
-                const valueBitcoinFor = new Intl.NumberFormat('en-US',{
-                    style: 'decimal',
-                    minimumFractionDigits: 8,
-                    maximumFractionDigits: 8
-                }).format(valueBitcoin)
-                foreignerValue.innerHTML = valueBitcoinFor
-            }
-        //Caso um valor não seja Digitado
+            //Caso um valor não seja Digitado
         } else {
-            brazilianReal.innerHTML = "Digite um Valor!!!"
+            valueConvertsIt.innerHTML = "Digite um Valor!!!"
         }
     } catch (err) {
         console.error("Erro" + err)
@@ -73,36 +152,65 @@ function convertsValues() {
 //Botão
 const button = document.querySelector("#button")
 //Select para a Mudança de Valor
-const select = document.querySelector("#select")
+const secondSelect = document.querySelector("#second-select")
+//Select converte de
+const primarySelect = document.querySelector("#primary-select")
 //Icone
 const icone = document.querySelector('#icone')
-//Texto do Icone
-const foreignerParagraph  = document.querySelector("#foreigner-paragraph")
+//Icone do Primeiro Select
+const iconeOne = document.querySelector("#icone-one")
+//Texto do Icone secound
+const foreignerParagraph = document.querySelector("#foreigner-paragraph")
+//Texto do Icone Primary
+const primaryParagraph = document.querySelector("#primary-paragraph")
 
 //Quando clica no Botão irá executar a Function
 button.addEventListener('click', convertsValues)
 
 //Mudança do Select 
-select.addEventListener('change', function () {
-    if (select.value === "USD") {
-        icone.src = "./assets/img/Logo USA.png"
-        foreignerParagraph.innerHTML = "Dolár amerciano"
+
+
+primarySelect.addEventListener('change', function () {
+    if (primarySelect.value === "BRL") {
+        iconeOne.src = "./assets/img/Logo Brasil.png"
+        primaryParagraph.innerHTML = "Real"
     }
-    if (select.value === "EUR") {
+    if (primarySelect.value === "USD") {
+        iconeOne.src = "./assets/img/Logo USA.png"
+        primaryParagraph.innerHTML = "Dólar"
+    }
+    if (primarySelect.value === "EUR") {
+        iconeOne.src = "./assets/img/Logo Euro.png"
+        primaryParagraph.innerHTML = "Euro"
+    }
+    convertsValues()
+})
+
+secondSelect.addEventListener('change', function () {
+    if (secondSelect.value === "BRL") {
+        icone.src = "./assets/img/Logo Brasil.png"
+        foreignerParagraph.innerHTML = "Real"
+    }
+    if (secondSelect.value === "USD") {
+        icone.src = "./assets/img/Logo USA.png"
+        foreignerParagraph.innerHTML = "Dolár"
+    }
+    if (secondSelect.value === "EUR") {
         icone.src = "./assets/img/Logo Euro.png"
         foreignerParagraph.innerHTML = "Euro"
     }
-    if (select.value === "LIB") {
+    if (secondSelect.value === "LIB") {
         icone.src = "./assets/img/Logo Libra.png"
         foreignerParagraph.innerHTML = "Libra"
     }
-    if(select.value === 'BIT'){
+    if (secondSelect.value === 'BIT') {
         icone.src = "./assets/img/Logo Bitcoin.png"
         foreignerParagraph.innerHTML = "Bitcoin"
     }
     //Chamando a função de converte Valores
     convertsValues()
 })
+
 
 
 
